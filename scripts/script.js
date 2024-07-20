4b6ebc9cc9881ba64488f8299f0acf06b040fcae
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-07-20 20:47:32 +0600

    practiced explicit types

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,28 @@
 "use strict";
-const divide = (a, b) => {
-    return a / b;
-};
-console.log(divide(24, 6));
+// const divide = (a: number, b: number): number => {
+//     return a / b;
+// }
+// console.log(divide(24, 6));
+// array
+// const fruits = ['apple', 'banana', 'orange', 5];
+// fruits.push(35);
+// const people: (number | string)[] = [];
+// people.push(3);
+// people.push('tititi');
+// // primitive
+// let a: string;
+// let b: boolean;
+// let c: number;
+// let player: {
+//     name: string,
+//     age: number,
+//     skills: Function,
+//     active: boolean
+// }
+// player = {
+//     name: "Abul",
+//     age: 44,
+//     skills: function () { console.log(this.age) },
+//     active: true
+// }
+// player.skills();
